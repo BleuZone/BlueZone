24970f240565b46a0e026123827947612445d028
Zachary Lewitton <zachary.lewitton@gmail.com>
2021-10-17 22:34:53 -0400

    Fixed schema errors and added mysql connection

--- db/schema.sql
+++ db/schema.sql
@@ -1,39 +1,46 @@
+CREATE DATABASE
+
 CREATE TABLE users (
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL,
   user_email varchar(255),
-  user_password varchar(255)
+  user_password varchar(255),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE username_matching (
-  id FOREIGN KEY REFERENCES users(id),
-  username varchar(255) PRIMARY KEY,
+  FOREIGN KEY (id) REFERENCES users(id),
+  username varchar(255),
   points INT,
+  PRIMARY KEY (username)
 );
 
 CREATE TABLE posts (
-  post_id INT NOT NULL PRIMARY KEY,
+  post_id INT NOT NULL,
   post_title varchar(255),
   post_body varchar(255) NOT NULL,
   points INT NOT NULL,
-  page_id FOREIGN KEY REFERENCES pages(page_id)
+  FOREIGN KEY page_id REFERENCES pages(page_id)
   creation_time date NOT NULL,
   comment_count int NOT NULL,
-  username FOREIGN KEY REFERENCES username_matching(username)
+  FOREIGN KEY (username)  REFERENCES username_matching(username),
+  PRIMARY KEY (post_id)
 );
 
 CREATE TABLE pages (
-  page_id INT NOT NULL PRIMARY KEY,
+  page_id INT NOT NULL,
   page_title varchar(255),
   parent_page boolean,
-  page_parent_id INT
+  page_parent_id INT,
+  PRIMARY KEY (page_id)
 );
 
 CREATE TABLE comments (
-  comment_id INT NOT NULL PRIMARY KEY,
-  username FOREIGN KEY REFERENCES username_matching(username)
+  comment_id INT NOT NULL,
+  FOREIGN KEY (username) REFERENCES username_matching(username)
   comment varchar(255),
   parent_exists boolean,
   parent_id INT,
-  post_id FOREIGN KEY REFERENCES posts(post_id)
+  FOREIGN KEY (post_id) REFERENCES posts(post_id)
   creation_time date NOT NULL
+  PRIMARY KEY (comment_id)
 );
