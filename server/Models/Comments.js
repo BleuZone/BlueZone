341516a716de0c6a2bd913e53835c75bf368ebad
Zachary Lewitton <zachary.lewitton@gmail.com>
2021-10-24 01:13:16 -0400

    Half way through the comment model

--- server/Models/Comments.js
+++ server/Models/Comments.js
@@ -2,9 +2,41 @@ let database = require('../../db/index.js');
 
 let getComments = (post_id, callback) => {
   //commentChain with fake data: [comment, username, points, comment_id, {subcomments: [subcomment1, user, points, subcomments: {sub-subcomment1, }]}]
-  let commentChain = []
-  let organizeComments = (parent_id, level) => {
-    database.query(`SELECT * FROM comments WHERE pose_id = ${post_id} AND `)
-  }
-  database.query(`SELECT * FROM comments WHERE post_id = ${post_id} ORDER BY points DESC`)
+  let commentObject = {
+    username: '',
+    comment: '',
+    points: '',
+    creationTime: '',
+    subComments: {}
+  };
+  // let commentChain = []
+  // let organizeComments = (parent_id, level) => {
+  //   database.query(`SELECT * FROM comments WHERE post_id = ${parent_id}`, (err, result) => {
+  //     if (err) {
+  //       return;
+  //     } else {
+
+  //     }
+  //   })
+  // }
+  database.query(`SELECT * FROM comments WHERE post_id = ${post_id} ORDER BY points DESC`, (err, result) => {
+    if(err) {
+      callback(err, null)
+    } else {
+      console.log("The result object is: ", result);
+      for (let x = 0; x < result.length; x++) {
+        console.log(result[x].comment_id);
+      }
+      // let data = JSON.parse(JSON.stringify(result));
+      // console
+    }
+  });
 }
+
+getComments(2, (err, result) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result)
+  }
+})
