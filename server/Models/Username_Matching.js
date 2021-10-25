80f09c7980511a8676aaa553029e9f50a05db704
Arjun Rao <ar482@duke.edu>
2021-10-24 22:20:26 -0400

    create username function

--- server/Models/Username_Matching.js
+++ server/Models/Username_Matching.js
@@ -9,8 +9,23 @@ let fetchUsername = (user_id, callback) => {
         }
       });
 }
+let createUsername = (user_id, username, callback) => {
+    database.query(`INSERT INTO username_matching(id, username, points) VALUES (${user_id}, '${username}', ${0})`, (err, result) => {
+        if(err){
+            callback(err, null);
+        }else{
+            callback(null, result);
+        }
+    });
+}
 
-
+createUsername(0, 'arjunrao', (err, result) => {
+    if(err){
+        console.log(err);
+    }else{
+console.log(result);
+    }
+});
 
 fetchUsername(0, (err, result) => {
     if(err){
