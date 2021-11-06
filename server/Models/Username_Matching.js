fad00e95f2c4051e5377197f3c356cc1e47861e6
Arjun Rao <ar482@duke.edu>
2021-11-06 19:38:34 -0400

    added javadoc comments

--- server/Models/Username_Matching.js
+++ server/Models/Username_Matching.js
@@ -1,5 +1,11 @@
 let database = require('../../db/index.js');
 
+/**
+ * 
+ * @param {*} user_id of username
+ * @param {*} callback 
+ * Fetch username for specific user id from username_matching table
+ */
 let fetchUsername = (user_id, callback) => {
     database.query(`SELECT * FROM username_matching WHERE id= ${user_id}`, (err,result) => {
         if (err) {
@@ -9,6 +15,14 @@ let fetchUsername = (user_id, callback) => {
         }
       });
 }
+
+/**
+ * 
+ * @param {*} user_id of username
+ * @param {*} username to be created
+ * @param {*} callback 
+ * Create a username associated to a user id in username_matching table
+ */
 let createUsername = (user_id, username, callback) => {
     database.query(`INSERT INTO username_matching(id, username, points) VALUES (${user_id}, '${username}', ${0})`, (err, result) => {
         if(err){
@@ -19,6 +33,13 @@ let createUsername = (user_id, username, callback) => {
     });
 }
 
+/**
+ * 
+ * @param {*} user_id of username
+ * @param {*} username to be changed
+ * @param {*} callback 
+ * update a username for a specific user id in username_matching table
+ */
 let changeUsername = (user_id, username, callback) => {
     database.query(`UPDATE username_matching SET username = '${username}' WHERE id = ${user_id}`, (err, result) => {
         if (err) {
@@ -29,8 +50,13 @@ let changeUsername = (user_id, username, callback) => {
     });
 }
 
-
-//this method has not been merged yet
+/**
+ * 
+ * @param {*} id of username
+ * @param {*} callback 
+ * Delete the username for a specific user id in username_matching table
+ * This deletes the username but does not update to (deleted) in table
+ */
 let deleteUsername = (id, callback) => {
     database.query(
         ` DELETE FROM username_matching WHERE id="${id}"`,
