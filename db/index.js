5ec4cd5445974c66a0465518cb0f9aa52c8b753b
Zachary Lewitton <zachary.lewitton@gmail.com>
2021-11-24 22:41:17 -0600

    Added a .env folder so that none of the database stuff if fixed into the repo

--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 let mysql = require('mysql');
 let connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'PASSWORD',
-  database: 'BlueZone'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 })
 
 connection.connect();
