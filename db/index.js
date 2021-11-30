1b81963e01545376eaf54ae055d207285a372cfa
Jodi Yeh <jhy9@duke.edu>
2021-11-30 04:06:48 -0500

    docker compose works and creates a container that runs both, need to port foward to show the hello world

--- db/index.js
+++ db/index.js
@@ -3,8 +3,8 @@ let mysql = require('mysql');
 let connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'PASSWORD',
-  database: process.env.DB_DATABASE || 'BlueZone'
+  password: process.env.DB_PASSWORD || 'host',
+  database: process.env.DB_DATABASE || 'blueZone'
 })
 
 connection.connect();
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express');
-const router = require('./Router/router.js');
+const router = require('./server/Router/router.js');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT;
