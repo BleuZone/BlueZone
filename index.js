5961756f355f7052f4de9f494fafabfa06ae7286
Jodi Yeh <jhy9@duke.edu>
2021-11-30 03:25:52 -0500

    docker compose w full index

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-const app = require('express')();
+require('dotenv').config()
+const express = require('express');
+const router = require('./Router/router.js');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT;
 
-app.get('/', (req, res ) => 
-    res.json({ message: 'Docker is easy 🐳' }) 
-);
 
-const port = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
-app.listen(port, () => console.log(`app listening on http://localhost:${port}`) );
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', `http://localhost:8080`);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+});
+
+app.use((req, res, next) => {
+  if (req.headers.authorization === process.env.APIKEY) {
+    next()
+  } else {
+    res.status(403).send({error: 'Invalid API Key'})
+  }
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use(router);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
