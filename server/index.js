2d72d8b819f836a9a57e2f0b9377ba3fe8d9bed0
Jodi Yeh <jhy9@duke.edu>
2021-12-06 16:15:26 -0500

    docker compose app listens

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', `http://localhost:3001`);
+  res.setHeader('Access-Control-Allow-Origin', `http://localhost:8080`);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   next();
